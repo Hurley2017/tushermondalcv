67f8efa65cf0d59430de8e9c6e5e45d047402541
Hurley2017 <luciefer9062hurley@gmail.com>
2026-08-14 05:19:25 +0530

    Remove social links from footer (keep Get CV)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,19 +54,6 @@ export default function Footer() {
           </button>
         </div>
         <div className="footer__icons">
-          {profile.socials.map((s) => (
-            <a
-              key={s.label}
-              className="social-btn social-btn--sm"
-              href={s.url}
-              target="_blank"
-              rel="noreferrer"
-              aria-label={s.label}
-              title={s.label}
-            >
-              <Icon name={s.icon} size={16} />
-            </a>
-          ))}
           <a className="btn btn--primary btn--sm footer__cv" href={profile.cvPdf} download>
             <Icon name="download" size={14} /> Get CV
           </a>
